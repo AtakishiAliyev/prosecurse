967c3433496df502f9536270225220fb91021d4e
atakishialiyev <atakishieliyev@gmail.com>
2022-02-10 00:07:48 +0400

    Done

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const burgerButton = document.querySelector('#menu');
+const navBar = document.querySelector('.navBar');
+
+burgerButton.addEventListener('click', () => {
+    console.log('show');
+    navBar.classList.toggle("show");
+}) 
